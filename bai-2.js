ae7d85fc639f490bd37ef87d003f9c13bf352958
Dam-Thanh-Hoa <damhoa654@gmail.com>
2020-09-10 16:56:16 +0700

    bt

---
+++ bai-2.js
@@ -0,0 +1,4 @@
+let temperature = prompt('nhap do C')
+let temperature_C = parseInt(temperature);
+let degreefahrenheit = (temperature*5/9)+32;
+document.write('degreefahrenheit =' + degreefahrenheit);
